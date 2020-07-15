7b3469c56de40074975ff322882d0eef7bb110cf
christian460 <64232962+christian460@users.noreply.github.com>
2020-07-14 20:12:08 -0500

    Update urls.py

--- src/ropa/urls.py
+++ src/ropa/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 urlpatterns=[
 	path("", views.index, name="index"),
-	path("index", views.regreso, name="index")
+	path("index", views.regreso, name="index"),
+	path("login", views.login, name="login"),
+	path("regisropa", views.regisropa, name="regisropa"),
+	path('cliente/', include('cliente.urls')),
 ]
