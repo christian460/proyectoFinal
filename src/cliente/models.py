e201ee4ac915b3271e67d045ca1411292e82711d
christian460 <64232962+christian460@users.noreply.github.com>
2020-07-15 23:08:40 -0500

    Add files via upload

---
+++ src/cliente/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+# Create your models here.
+
+class Cliente(AbstractUser):
+	telefono=models.IntegerField(default=0)
+	direccion=models.CharField(max_length=100, default=' ')
+		
---
+++ src/cliente/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User, auth
+# Create your views here.
+
+def registro(request):
+	if request.method == 'POST':
+		nombre = request.POST['first_name']
+		apellidos = request.POST['last_name']
+		email = request.POST['email']
+		usuario = request.POST['username']
+		contraseña1 = request.POST['password1']
+		contraseña2 = request.POST['password2']
+		telefono = request.POST['telefono']
+		direccion = request.POST['direccion']
+		User=get_user_model()
+		if contraseña1==contraseña2:
+			if User.objects.filter(username=usuario).exists():
+				messages.info(request,'Este usuario ya existe')
+				return redirect('registro')
+			elif User.objects.filter(email=email).exists():
+				messages.info(request,'Este email ya existe')
+				return redirect('registro')
+			else: 	
+				user= User.objects.create_user(username=usuario, password=contraseña1, email=email, first_name=nombre, last_name=apellidos, telefono=telefono, direccion=direccion)
+				user.save();
+				print ('Usuario creado')
+				return redirect('login')
+		else:
+			messages.info(request, 'Las contraseñas no coinciden')
+			return redirect('register')
+		return redirect('/')
+	else:
+		return render(request,'registro.html')
+
+def login(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = auth.authenticate(username=username,password=password)
+
+		if user is not None:
+			auth.login(request,user)
+			return redirect("/")
+		else:
+			messages.info(request,'Datos erroneos')
+			return redirect('login')
+	else:
+		return render(request,'login.html')
+
+def cerrarSesion(request):
+	auth.logout(request)
+	return redirect('/')
+
+def regreso(request):
+	return redirect('/')
+
+
