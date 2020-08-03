acfaadc5932633bda21c409d04ed0862823dcd4a
christian460 <64232962+christian460@users.noreply.github.com>
2020-08-03 18:15:16 -0500

    Add files via upload

--- src/ropa/views.py
+++ src/ropa/views.py
@@ -4,7 +4,8 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-	return render(request,"index.html")
+	rops=Ropa.objects.all()
+	return render(request,"index.html", {'rops':rops})
 
 def regreso(request):
 	return redirect('/')
@@ -16,9 +17,8 @@ def regisropa(request):
 	if request.method == 'POST':
 		nombre = request.POST['nombre']
 		modelo = request.POST['modelo']
-		color = request.POST['color']
-		material = request.POST['material']
-		talla = request.POST['talla']
+		des = request.POST['des']
+		cat = request.POST['cat']
 		pre=request.POST['pre']
 		prom=request.POST['prom']
 		rops=Ropa.objects.all()
@@ -26,7 +26,7 @@ def regisropa(request):
 			if rop.nombre!=nombre and rop.modelo!=modelo:
 				messages.info(request,'Esta prenda ya existe')
 				return redirect('regisropa')
-			ropa=Ropa.objects.create(nombre=nombre, modelo=modelo, color=color, material=material, talla=talla, pre=pre, prom=prom)
+			ropa=Ropa.objects.create(nombre=nombre, modelo=modelo, des=des, cat=cat, pre=pre, prom=prom)
 			ropa.save();
 			return redirect('/')
 	else:
