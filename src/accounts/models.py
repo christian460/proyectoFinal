3c96e5574131c37e86b6884d914934dc5f08d1fb
LuisFernando <lquispepum@unsa.edu.pe>
2020-08-12 22:16:45 -0500

    Creando el modelo Perfil

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import (
     AbstractBaseUser,BaseUserManager
 )
+from django.db.models.signals import post_save
+from wear.models import Wear
+from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -92,5 +97,15 @@ class User(AbstractBaseUser):
     def is_admin(self):
         return self.admin
     
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wears = models.ManyToManyField(Wear, blank=True)
 
+    def __str__(self):
+        return self.user.email
 
+def post_save_profile_create(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+post_save.connect(post_save_profile_create, sender=User)
