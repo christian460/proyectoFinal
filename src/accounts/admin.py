d8fce21a275694002341bfc6998abb81f95a7d9b
LuisFernando <lquispepum@unsa.edu.pe>
2020-08-12 22:16:22 -0500

    Crando la app Profile

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import Profile
 # Register your models here.
 
 User = get_user_model()
@@ -10,3 +11,4 @@ class UserAdmin(admin.ModelAdmin):
         model = User
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
